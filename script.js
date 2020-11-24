8dd7953725c1699153e591028a9d02dbe1217819
Donnahue George <Donnahuegjr@gmail.com>
2020-11-24 10:18:39 -0500

    first commit with pseudo code and base code

---
+++ script.js
@@ -0,0 +1,26 @@
+//variables
+var timeLeft
+var timeDisplay
+var currentHighscore
+var questionArray = []
+var answerArray20 = []
+var answerArray1 = []
+var answerArray2 = []
+var answerArray3 = []
+
+
+// function to display timer
+
+
+//function to start the game 
+
+
+
+//function to display questions and interaction with right and wrong answers
+
+
+
+
+// function to show how the final score and to enter high score
+
+//restart button function
