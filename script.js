3993bc3433f8cb7b424b73821f28db6d9f9e5e27
Donnahue George <Donnahuegjr@gmail.com>
2020-12-02 22:07:28 -0500

    end game screen in the making

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //variables
 var timeLeft = $("#timer")
-var timeDisplay = 60
+var timeDisplay = 5
 var currentHighscore = 0
 var questionArray = ["Q0", "Q1", "Q2", "Q3","Q4"]
 var answerArray0 = ["A0-1", "A0-2", "A0-3", "A0-4"]
@@ -10,16 +10,17 @@ var answerArray3 = ["A3-1", "A3-2", "A3-3", "A3-4"]
 var answerArray4 = ["A4-1", "A4-2", "A4-3", "A4-4"]
 var questionsEl = $("#Qdisplay")
 var questionMove = false
-arrayQu=0
+arrayQu= 0
+var saveScore = $("#gameResults")
+var score = $("#score")
 //display timer
 timeLeft.text(timeDisplay)
 console.log(questionMove)
-
+console.log(answerArray0)
 //function to start the game 
 $("#readyButton").on("click", function() {
     $("#begin").css("display", "none")
     questionsEl.css("display", "block")
-    timeDisplay = 60
     questionStrings()
     //countdown timer
     interval = setInterval(function(){
@@ -30,8 +31,7 @@ $("#readyButton").on("click", function() {
         }
         else if (timeDisplay == 0){
             clearInterval(interval)
-            console.log("time ended")
-            endQuiz()
+           return endQuiz()
         }
     }, 1000)    
 })
@@ -39,29 +39,56 @@ $("#readyButton").on("click", function() {
 
 $("#ques").text(questionArray[arrayQu])
 function questionStrings(){
-    $(".ansButton").on("click", function() {
-        questionMove = !questionMove
-         console.log(questionMove)
-        
 
+    for ( i = 0; i < answerArray0.length; i++) {
+        $("<li>").append(answerArray0[i])
+        console.log(answerArray0)
+    }
+    $("#ans2").on("click", function() {
+         console.log(questionMove)
+            
+         var answerPool = answerArray0
+            if (arrayQu = 0) {
+                answerPool = answerArray0
+            }
+            else if (arrayQu = 1) {
+                answerPool = answerArray1
+            }
+    
+            else if (arrayQu = 2) {
+                answerPool = answerArray2
+            }
+            else if (arrayQu = 3) {
+                answerPool = answerArray3
+            }
+            console.log(arrayQu)
+            console.log(answerPool)
+        
         if (questionMove === true) {
-            arrayQu++
+            arrayQu+
             $("#ques").text(questionArray[arrayQu])
             console.log(questionArray[arrayQu])
-            console.log("Am I WORKING!!")
+            questionMove = false
         }   
-        // if(arrayQu == 1) {
-        // questionMove = false
-        // console.log(questionMove)
-        // }
+        else {
+            questionMove = false
+             console.log(questionMove)
+            }
+
     })
-
-
-
-
-
 }
+// doing question and answers another day mind is fried lol
+
+function endQuiz() {
+    questionsEl.css("display", "none")
+    saveScore.css("display", "block")
+    score.text(timeDisplay)
+    
+    }
+
+
 // function to show how the final score and to enter high score
+
 
 //restart button function
 
